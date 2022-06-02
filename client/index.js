073a86227f83e7f55e4213a44732ffd9f8d85a97
Kirill Zhukau <67194208+kotikkir9@users.noreply.github.com>
2022-06-02 02:57:04 +0200

    made some dirs and cleaned up a lil

---
+++ client/index.js
@@ -0,0 +1,56 @@
+let socketOpen = false;
+
+const button = document.querySelector("#send"),
+    discButton = document.querySelector("#disconnect"),
+    output = document.querySelector("#output"),
+    textarea = document.querySelector("textarea"),
+    wsUri = "ws://127.0.0.1/",
+    websocket = new WebSocket(wsUri);
+
+button.addEventListener("click", onClickButton);
+discButton.addEventListener("click", () => {
+    websocket.close();
+});
+
+websocket.onopen = function (e) {
+    socketOpen = true;
+    writeToScreen("CONNECTED");
+    doSend("WebSocket rocks");
+};
+
+websocket.onclose = function (e) {
+    socketOpen = false;
+    writeToScreen("DISCONNECTED");
+};
+
+websocket.onmessage = function (e) {
+    writeToScreen("<span>RESPONSE: " + e.data + "</span>");
+};
+
+websocket.onerror = function (e) {
+    writeToScreen("<span class=error>ERROR:</span> " + e.data);
+};
+
+function doSend(message) {
+    if (socketOpen) {
+        writeToScreen("SENT: " + message);
+        websocket.send(message);
+    } else {
+        writeToScreen("Connection closed!");
+    }
+}
+
+function writeToScreen(message) {
+    output.insertAdjacentHTML(
+        "afterbegin",
+        "<p>" + message + "</p>"
+    );
+}
+
+function onClickButton() {
+    var text = textarea.value;
+
+    text && doSend(text);
+    textarea.value = "";
+    textarea.focus();
+}
