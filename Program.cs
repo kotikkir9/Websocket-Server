74fd5fbed3aa77b8dd6991b28a41e9fe032fcc0e
Kirill Zhukau <67194208+kotikkir9@users.noreply.github.com>
2022-06-05 16:12:37 +0200

    removed TODO list

--- Program.cs
+++ Program.cs
@@ -2,7 +2,3 @@
 
 Server server = new Server();
 server.start();
-
-// TODO: store all connections in a list
-// TODO: send messages to all connections when a new connection is established
-// TODO: remove connections from the list when they disconnect
